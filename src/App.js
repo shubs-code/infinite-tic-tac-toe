9aa77e892a5a08e2d93733cb8ac4718000daf064
shubs-code <shubs.code@gmail.com>
2024-07-20 17:59:01 +0530

    centering board on large screen

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ function App() {
   return (
     <div className="h-screen flex flex-col p-4 App">
       <div className="grow bg-black p-4 rounded-md">
-        <div className="mb-2 text-center text-white text-lg">Tic Tac Toe</div>
+        <div className="mb-2 lg:mb-4 text-center text-white text-lg">Tic Tac Toe</div>
         <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3">
-          <Board/>
+          <div className="hidden lg:block"></div>
+          <div className=""><Board/></div>
         </div>
       </div>
     </div>
